7717045982bf27b172e2f142cf2c6c4ba1bba319
rsantana <richardsantana83088@gmail.com>
2019-11-06 22:25:35 -0600

    testing to see if i can pusht github

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
-package main
+package main 
 
-import "fmt"
+import ("fmt"
+		"github.com/r-santana/stringutil"
+		
+		)
 
 func main(){
-	fmt.Printf("hello, world\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH")+",rev func in stringutil package\n")
+	fmt.Printf(stringutil.Richie("!boj ,doog")+",rich func in stringutil package\n")
+	rsantan()
+	
 }
+func rsantan(){
+	fmt.Printf("rsantan func in main (local) package\n") 
+}	
+//asdfasd
